9106c9b3d6f3fc508722b462e299c62289d785f5
leeboo <leeboo@yidianhulian.com>
2021-04-11 20:35:23 +0800

    feat 删除缓存功能

--- yangzie/welcome.tpl.php
+++ yangzie/welcome.tpl.php
@@ -2,7 +2,7 @@
 namespace yangzie;
 ?>
 <h1>
-Yangzie by <a target="_blank" href="http://yidianhulian.com">YDHL</a><sup>&copy;</sup> at GuiYang</h1>
+    轻量级php开发框架，来自 <a target="_blank" href="http://yidianhulian.com">易点互联</a></h1>
 创建代码
 <ol>
 	<li>打开cmd，进入到<?php echo realpath(YZE_INSTALL_PATH)?></li>
